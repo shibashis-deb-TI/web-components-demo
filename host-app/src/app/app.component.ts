4654c85e1cdef2146686a4f527706bd34a3c4733
Shibashis Deb <shibashis.deb@technoidentity.com>
2024-05-22 23:30:00 +0530

    add urls to scripts in host; copy build files to assets in mfe

--- host-app/src/app/app.component.ts
+++ host-app/src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from "@angular/common";
+import { HttpClient } from "@angular/common/http";
+import { Component, Inject, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'host-app';
+export class AppComponent implements OnInit {
+  title = "host-app";
+
+  constructor(
+    private http: HttpClient,
+    @Inject(DOCUMENT) private document: Document
+  ) {}
+
+  ngOnInit(): void {
+    // this.http
+    //   .get("http://localhost:4300/main.js", {
+    //     responseType: "text",
+    //   })
+    //   .subscribe((main) => {
+    //     console.log(main);
+    //     const script = this.document.createElement("script");
+    //     script.type = "module";
+    //     if (script) {
+    //       script.innerHTML = main;
+    //       // script.style.display = "block";
+    //       this.document.body.appendChild(script);
+    //     }
+    //   });
+    // this.http
+    //   .get("http://localhost:4300/polyfills.js", {
+    //     responseType: "text",
+    //   })
+    //   .subscribe((main) => {
+    //     console.log(main);
+    //     const script = this.document.createElement("script");
+    //     script.setAttribute("type", "module");
+    //     if (script) {
+    //       script.innerHTML = JSON.parse(main);
+    //       script.style.display = "block";
+    //       this.document.body.appendChild(script);
+    //     }
+    //   });
+    // this.http
+    //   .get("http://localhost:4300/runtime.js", {
+    //     responseType: "text",
+    //   })
+    //   .subscribe((main) => {
+    //     console.log(main);
+    //     const script = this.document.createElement("script");
+    //     script.setAttribute("type", "module");
+    //     if (script) {
+    //       script.innerHTML = JSON.parse(main);
+    //       script.style.display = "block";
+    //       this.document.body.appendChild(script);
+    //     }
+    //   });
+    // this.http
+    //   .get("http://localhost:4300/styles.css", {
+    //     responseType: "text",
+    //   })
+    //   .subscribe((main) => {
+    //     console.log(main);
+    //     const link = this.document.getElementById("style");
+    //     if (link) {
+    //       link.innerHTML = JSON.parse(main);
+    //     }
+    //   });
+  }
 }
